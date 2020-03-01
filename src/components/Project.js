aa03b32122cb9d1f4522d0fa77e23c669eabba16
KHong <albtrossfacefall@gmail.com>
2020-03-01 09:36:48 -0500

    add expand project view, add tech list

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import projectStyles from '../styles/Project.module.scss'
+
+const ExpandedProject = ({active}) => {
+  if (active) {
+    return (
+      <div className={projectStyles.expandedProject}>
+        expanded
+      </div>
+    )
+  } else {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
 
 const Project = ({
   informListStatus, projectId, active, 
@@ -11,12 +26,18 @@ const Project = ({
     informListStatus(active ? 0 : projectId);
   }
 
-  // const getColor = () => active ? 'green' : 'pink'
-
   return (
     <div className={projectStyles.projectTitleContainer} onClick = {handleClick}>
-      <h3 className={projectStyles.title}> {name}</h3>
-      <p className={projectStyles.description}> {description}</p>
+      <h3 className={projectStyles.title}> {name} </h3>
+      <p className={projectStyles.description}> {description} </p>
+
+      <div className={projectStyles.techList}>
+        {tech.map(string => (
+          <div className={projectStyles.tech}> {string} </div>
+        ))}
+      </div>
+
+      <ExpandedProject active={active}/>
     </div>
   )
 }
