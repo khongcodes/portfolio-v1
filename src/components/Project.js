1ae8cccf984d2c40071a338ba91fde3810214b11
KHong <albtrossfacefall@gmail.com>
2020-03-02 14:36:43 -0500

    fix broken prop transfer in ProjectsList

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 import projectStyles from '../styles/Project.module.scss'
 
-const ExpandedProject = ({active}) => {
+const ExpandedProject = ({active, name, img, git, live, youtube}) => {
   if (active) {
+    console.log(img)
+    console.log(git)
+    console.log(live)
+    console.log(youtube)
     return (
       <div className={projectStyles.expandedProject}>
-        expanded
+        <img src={img} alt={name}/>
       </div>
     )
   } else {
@@ -18,13 +22,13 @@ const ExpandedProject = ({active}) => {
 }
 
 const Project = ({
-  informListStatus, projectId, active, 
+  informListStatus, id, active,
   name, description, tech,
-  imgPath, git, live, youtube
+  ...expandedProjData
 }) => {
 
   const handleClick = () => {
-    informListStatus(active ? 0 : projectId);
+    informListStatus(active ? 0 : id);
   }
 
   return (
@@ -37,11 +41,15 @@ const Project = ({
 
       <div className={projectStyles.techList}>
         {tech.map(string => (
-          <div className={projectStyles.tech}> {string} </div>
+          <div key={string} className={projectStyles.tech}> {string} </div>
         ))}
       </div>
 
-      <ExpandedProject active={active}/>
+      <ExpandedProject 
+        active = {active}
+        name = {name}
+        {...expandedProjData}
+      />
     </div>
   )
 }
--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -3,17 +3,26 @@ import React, { useState } from 'react';
 import Project from './Project';
 import projectData from '../data/projectData';
 
+import invitationImg from '../images/invitation.png';
+import wanderingImg from '../images/wandering.png';
+import heroImg from '../images/hero-s-journey-spread.png';
+import itchImg from '../images/itchscraper.png';
+
 const ProjectsList = () => {
   const [selectedProject, setSelectedProject] = useState(0)
 
   const listCallback = id => setSelectedProject(id)
 
+  const imgArray = [invitationImg, wanderingImg, heroImg, itchImg]
+
   return (
     <div>
-      {projectData.projects.map(proj => (
-        <Project 
+      {projectData.projects.map((proj, index) => (
+        <Project
+          key = {proj.name}
           informListStatus = {listCallback}
           active = {selectedProject === proj.id}
+          img = {imgArray[index]}
           {...proj}
         />
       ))}
