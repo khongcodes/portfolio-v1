27a2708fb571fa93bc2d73a7b5681cb86e15a8c6
KHong <albtrossfacefall@gmail.com>
2020-03-11 15:20:25 -0400

    add favicon and use react helmet to pass metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,21 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/favicon_io/icon.png`, // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
 import ProjectsList from "../components/ProjectsList";
 
-import Layout from "../components/Layout";
 import indexStyles from "../styles/index.module.scss";
 
 const IndexPage = () => (
   <Layout>
+    <SEO title="Home"/>
     <div className={indexStyles.introSection}>
       <h1>Kevin Hong</h1>
       <p>Full stack web developer & designer</p>
